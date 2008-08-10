942e72591ea76ebeea79d2ea6d8d32403f611535
Neil Roberts <neil@o-hand.com>
2008-08-10 21:04:38 +0100

    Added copyright messages

--- src/git-source-view.h
+++ src/git-source-view.h
@@ -1,3 +1,20 @@
+/* This file is part of blame-browse.
+ * Copyright (C) 2008  Neil Roberts  <bpeeluk@yahoo.co.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __GIT_SOURCE_VIEW_H__
 #define __GIT_SOURCE_VIEW_H__
 
