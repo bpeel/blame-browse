8a96079d59d3bf48e275ea29614bff47809fdccf
Neil Roberts <neil@linux.intel.com>
2011-02-11 10:36:57 +0000

    autogen.sh: Divert to gnome-autogen.sh instead
    There's no point in maintaining an autogen script when the generic
    gnome-autogen script works fine.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-autoreconf -v --install || exit $?
-./configure "$@"
+which gnome-autogen.sh > /dev/null 2>/dev/null;
+
+if test "$?" -eq "0"; then
+    exec gnome-autogen.sh "$@";
+fi;
+
+echo "Please install gnome-autogen.sh and use that instead.";
+
+exit 1;
